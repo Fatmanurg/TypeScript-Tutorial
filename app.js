d28c53c6530694b1c11dccb88f4415fdda57b386
Fatmanurg <fatmanurg.1997@gmail.com>
2019-12-11 15:32:48 +0300

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 "use strict";
 var str = 'Hello World!';
+var boolFalse = false;
+var boolTrue = true;
+var str1 = 'Hello World';
+var multitypeVar = 'String';
+multitypeVar = 20;
+var Human = /** @class */ (function () {
+    function Human(data) {
+        this.name = data;
+    }
+    return Human;
+}());
+var human = new Human('Gabriel');
+if (human instanceof Human) {
+    console.log(human.name + " is a human");
+}
+var str2 = 'I am a String';
+var strLength = str.length;
+var strings = ['Hello', 'World', '!'];
--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
 let str: string = 'Hello World!';
+let boolFalse: boolean = false;
+let boolTrue: boolean = true;
+let str1: string = 'Hello World'
+let multitypeVar: string | number = 'String'
+multitypeVar = 20
+class Human{
+    name: string;
+    constructor(data: string) {
+     this.name = data;
+    }
+   }
+   let human = new Human('Gabriel')
+   if(human instanceof Human){
+    console.log(`${human.name} is a human`)
+   }
+   let str2: any = 'I am a String'
+let strLength = (str as string).length
+
+let strings: string[] = ['Hello', 'World', '!']
